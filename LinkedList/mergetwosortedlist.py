8d1b5962f531cb6c926cd0e1f593534178e9c17b
Marianna-Martin <149689310+Marianna-Martin@users.noreply.github.com>
2024-07-31 23:59:34 +0530

    Create mergetwosortedlist.py

---
+++ LinkedList/mergetwosortedlist.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()  # Dummy node to simplify edge cases
+        current = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+
+    # If either list is not empty, attach it to the result
+        if l1:
+            current.next = l1
+        elif l2:
+            current.next = l2
+
+        return dummy.next
